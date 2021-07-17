fe64c217cdd5c1ee4c75142751fde9abfbbb5e85
Lisa Hammarstrand <lisa@Lisas-MacBook-Air.local>
2021-07-17 16:13:44 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Åre 2021",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "-DLZ4H60Cb3Zng4nMRK3uvmwaHfdaYLeEqILyFmj4ZQ",
+        spaceId: "mt0kvcyico1u",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
